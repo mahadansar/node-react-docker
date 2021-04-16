bb7af31edba2c8ea1524966382a62ef823fdd106
Mahad Ansar <mahadansar00@gmail.com>
2021-04-16 12:39:58 +0500

    .

--- reactp/src/App.js
+++ reactp/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   useEffect(() => {
     async function fetchData() {
-      let data = await axios.get("http://localhost:4000/t");
+      let data = await axios.get("http://localhost:4000/");
       setText(data.data.msg);
     }
     fetchData();
